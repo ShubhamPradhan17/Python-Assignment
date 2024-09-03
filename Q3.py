61f9b7e7a5a51e315ffe109bb907cd2367887e15
shubhampradhan1709 <shubham.pradhan17@gmail.com>
2024-09-03 15:11:48 +0530

    Code changes in the Q3.py related to yield

--- Q3.py
+++ Q3.py
@@ -1,19 +1,12 @@
 # Build a counter generator
 
 def counter():
-     yield 1
-     yield 2
-     yield 3
-     yield 4
-     yield 5
+    count = 1
+    while True:
+        yield count  
+        count += 1
 
 vals = counter()
 
-# print(vals.__next__())
-# print(vals.__next__())
-# print(vals.__next__())
-# print(vals.__next__())
-# print(vals.__next__())
-
-for i in vals:
-     print(i)
+for i in range(5):
+     print(next(vals))
